c06c3d75616021bba57cd174d260d02671de7742
Martín M <skydiver@users.noreply.github.com>
2019-01-17 19:19:48 -0300

    code cleanup

---
+++ app.js
@@ -0,0 +1,49 @@
+let userIsInFrontOfScreen = false;
+
+// Enable experimental features in Chrome
+// chrome://flags#enable-experimental-web-platform-features
+
+// Check whether we have FaceDetector API available
+if (window.FaceDetector !== undefined) {
+  // User must allow access to webcam
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function(stream) {
+      // Get video element for the webcam video stream
+      let webcamVideo = document.querySelector('#webcam');
+
+      // Get webcam canvas used for face detection
+      let webcamCanvas = document.querySelector('#webcam-canvas');
+      let canvasContext = webcamCanvas.getContext('2d');
+
+      // Attach webcam stream to video element
+      webcamVideo.srcObject = stream;
+      webcamVideo.play();
+
+      // Create FaceDetector API
+      let faceDetector = new window.FaceDetector();
+
+
+      // Detect face every 500 ms
+      setInterval(function() {
+        canvasContext.drawImage(webcamVideo, 0, 0, webcamCanvas.width, webcamCanvas.height);
+        faceDetector.detect(webcamCanvas).then(function(faces) {
+          if (faces.length > 0) {
+            if (!userIsInFrontOfScreen) {
+              userIsInFrontOfScreen = true;
+              document.querySelector('#watching-yes').className = 'monkey visible';
+              document.querySelector('#watching-no').className = 'monkey';
+            }
+          } else if (userIsInFrontOfScreen) {
+            userIsInFrontOfScreen = false;
+            document.querySelector('#watching-no').className = 'monkey visible';
+            document.querySelector('#watching-yes').className = 'monkey';
+          }
+        });
+      }, 500);
+
+    }).catch(function(err) {
+      console.error(err);
+    });
+} else {
+  console.log('FaceDetector API was not found, enable experimental features chrome://flags#enable-experimental-web-platform-features');
+}
