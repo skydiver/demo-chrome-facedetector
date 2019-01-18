e771b472652fc8481786f9cbf0459051b9ffc1d5
Martín M <skydiver@users.noreply.github.com>
2019-01-17 22:05:49 -0300

    added pig face

--- app.js
+++ app.js
@@ -23,22 +23,68 @@ if (window.FaceDetector !== undefined) {
       let faceDetector = new window.FaceDetector();
 
 
+
+      setInterval(async () => {
+
+        const result = await faceDetector.detect(webcamVideo);
+        const coords = result[0].boundingBox;
+        const square = document.querySelector('#square');
+
+
+        // const newWidth = coords.width * 1.3;
+        // const newLeft = coords.left - (newWidth - coords.width)
+
+
+
+        // const newHeight = coords.height * 1.3;
+        // const newTop = coords.top - (newHeight - coords.height)
+
+square.style.width = `${coords.width}px`;
+square.style.height = `${coords.height}px`;
+square.style.top = `${coords.top}px`;
+square.style.left = `${coords.left}px`;
+
+        // square.style.width = `${newWidth}px`;
+        // square.style.height = `${ newHeight}px`;
+
+
+        // square.style.top = `${ newTop}px`;
+        // square.style.left = `${newLeft}px`;
+        // square.style.right = `${coords.right}px`;
+        // square.style.bottom = `${ coords.bottom}px`;
+
+
+      }, 300);
+
+
+
+      // bottom: 309.35906982421875
+      // height: 129.9478759765625
+      // left: 170.14967346191406
+      // right: 300.09754943847656
+      // top: 179.41119384765625
+      // width: 129.9478759765625
+      // x: 170.14967346191406
+      // y: 179.41119384765625
+
+
+
       // Detect face every 500 ms
       setInterval(function() {
         canvasContext.drawImage(webcamVideo, 0, 0, webcamCanvas.width, webcamCanvas.height);
-        faceDetector.detect(webcamCanvas).then(function(faces) {
-          if (faces.length > 0) {
-            if (!userIsInFrontOfScreen) {
-              userIsInFrontOfScreen = true;
-              document.querySelector('#watching-yes').className = 'monkey visible';
-              document.querySelector('#watching-no').className = 'monkey';
-            }
-          } else if (userIsInFrontOfScreen) {
-            userIsInFrontOfScreen = false;
-            document.querySelector('#watching-no').className = 'monkey visible';
-            document.querySelector('#watching-yes').className = 'monkey';
-          }
-        });
+        // faceDetector.detect(webcamCanvas).then(function(faces) {
+        //   if (faces.length > 0) {
+        //     if (!userIsInFrontOfScreen) {
+        //       userIsInFrontOfScreen = true;
+        //       document.querySelector('#watching-yes').className = 'monkey visible';
+        //       document.querySelector('#watching-no').className = 'monkey';
+        //     }
+        //   } else if (userIsInFrontOfScreen) {
+        //     userIsInFrontOfScreen = false;
+        //     document.querySelector('#watching-no').className = 'monkey visible';
+        //     document.querySelector('#watching-yes').className = 'monkey';
+        //   }
+        // });
       }, 500);
 
     }).catch(function(err) {
